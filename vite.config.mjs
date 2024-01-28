d358a7d3f33e0274957dfbda2d303357f4c57f41
sasshu <winter12shu27@gmail.com>
2024-01-28 21:21:25 +0900

    アクションタイトルの変更と未使用フォントのコメントアウト

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@
 import Components from "unplugin-vue-components/vite";
 import Vue from "@vitejs/plugin-vue";
 import Vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-import ViteFonts from "unplugin-fonts/vite";
+// import ViteFonts from "unplugin-fonts/vite";
 
 // Utilities
 import { defineConfig } from "vite";
@@ -17,16 +17,16 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify(),
     Components(),
-    ViteFonts({
-      google: {
-        families: [
-          {
-            name: "Roboto",
-            styles: "wght@100;300;400;500;700;900",
-          },
-        ],
-      },
-    }),
+    // ViteFonts({
+    //   google: {
+    //     families: [
+    //       {
+    //         name: "Roboto",
+    //         styles: "wght@100;300;400;500;700;900",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   define: { "process.env": {} },
   resolve: {
