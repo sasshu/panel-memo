276b05cb8879a60af2c730a43f28ae76f32d94a8
sasshu <winter12shu27@gmail.com>
2026-03-27 23:04:53 +0900

    [update]初期画面の背景画像の大きさ調整（最大でも縦50%）

--- src/views/MemoList.vue
+++ src/views/MemoList.vue
@@ -42,7 +42,6 @@
       >
         <div class="bg-memolist-wrap">
           <v-img
-            width="100%"
             src="../assets/bg-memolist.svg"
             style="opacity: 0.5"
           ></v-img>
@@ -678,9 +677,8 @@ const handleChangeMemoColor = async (index, color) => {
 }
 
 .bg-memolist-wrap {
-  flex: 0 0 50%;
   width: 50%;
-  max-width: 50%;
+  max-height: 50%;
 }
 
 .card {
